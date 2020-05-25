d37a49bc01f908025219d902e2d444a7792f7710
imacar <im.acar82@gmail.com>
2020-05-25 17:08:08 +0300

    a commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Database {
    * @param {"json"|"level"} type Type of the database
    * @param {Object} options Options of the database
    * @param {String} options.databaseName Name of the database
-   * @param {Boolean} [options.autoFile=false] If file doesn't exits, create new one.
+   * @param {Boolean} [options.autoFile=false] If file doesn't exits, create new one. (only for "json" database)
    * @param {Boolean} [options.ignoreWarns=false] Ignore warns?
    * @param {Boolean} [options.readableSaving=false] Save data in readable format to json database?
   */
@@ -43,7 +43,8 @@ class Database {
    * @param {String} key
    * @returns {true}
    * @example
-   * db.delete("test.okey"); // 12
+   * db.set("test.okey", 12);
+   * db.delete("test.okey"); // true
   */
   delete(key) {
     if (typeof key == "undefined") throw new TypeError("\"key\" parameter must be available.");
@@ -58,6 +59,7 @@ class Database {
    * All data in created database.
    * @returns {Object}
    * @example
+   * db.set("test.okey", 12);
    * db.all(); // {"test": {"okey": 12}}
   */
   all() {
@@ -69,6 +71,7 @@ class Database {
    * @param {String} key
    * @returns {Number|String|Boolean|Object|Array}
    * @example
+   * db.set("test.okey", 12);
    * db.get("test"); // {"okey": 12}
   */
   get(key) {
@@ -83,6 +86,7 @@ class Database {
    * @param {String} key
    * @returns {Boolean}
    * @example
+   * db.set("test.okey", 12);
    * db.has("test"); // true
   */
   has(key) {
@@ -102,11 +106,12 @@ class Database {
   }
 
   /**
-   * Pushs data to created database.
+   * Pushs element to data in created database.
    * @param {String} key
    * @param {Number|String|Boolean|Object|Array} value
    * @returns {Array}
    * @example
+   * db.set("test.hello", []);
    * db.push("test.hello", 12); // [12]
   */
   push(key, value) {
@@ -189,6 +194,46 @@ class Database {
     return this.database.set(key, newData);
   }
 
+
+  /**
+   * Sets element from data in created database by index.
+   * @param {String} key
+   * @param {Number} index
+   * @param {Number|String|Boolean|Object|Array} value
+   * @returns {Array}
+   * @example
+   * db.get("test"); // [12, 21, 32]
+   * db.setByIndex("test", 2, "test lol"); // [12, 21, "test lol"]
+  */
+  setByIndex(key, index, value) {
+    if (typeof key == "undefined") throw new TypeError("\"key\" parameter must be available.");
+    if (typeof key != "string") throw new TypeError("\"key\" parameter must be String.");
+    if (typeof index == "undefined") throw new TypeError("\"index\" parameter must be available.");
+    if (typeof index != "number") throw new TypeError("\"index\" parameter must be Number.");
+    if (typeof value == "undefined") throw new TypeError("\"value\" parameter must be available.");
+    if (!isValidValue(value)) throw new TypeError("\"value\" parameter must be String or Number or Boolean or Object or Array.");
+
+    if ((this.database.has(key) == false) && (this.database.ignoreWarns == false)) console.warn("This data isn't available. Created new array to this data.");
+    if (this.database.has(key) == false) this.database.set(key, []);
+
+    let data = this.database.get(key);
+    let newData = [];
+
+    if (!Array.isArray(data)) throw new TypeError("This data isn't Array so couldn't delete element from this data.");
+
+    for (let i = 0; i < data.length; i++) {
+      let element = data[i];
+
+      if (i == index) {
+        newData.push(value);
+      } else {
+        newData.push(element);
+      }
+    }
+
+    return this.database.set(key, newData);
+  }
+
   /**
    * Updates data in created database.
    * @param {String} key
@@ -217,7 +262,7 @@ class Database {
    * @param {Number} value
    * @returns {Number}
    * @example
-   * db.get("test"); // 12
+   * db.set("test", 12);
    * db.add("test", 12); // 24
   */
   add(key, value) {
@@ -241,7 +286,7 @@ class Database {
    * @param {Number} value
    * @returns {Number}
    * @example
-   * db.get("test"); // 12
+   * db.get("test");
    * db.subtract("test", 12); // 0
   */
   subtract(key, value) {
